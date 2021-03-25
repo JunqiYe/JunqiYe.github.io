e2f1e806432826b1e7132eea96a58a40a720619b
Issac PC <jy98@uw.edu>
2021-03-25 01:25:36 -0700

    Add ReactJS library and JSX script and link two old projects on the homepage as a start

--- README.md
+++ README.md
@@ -35,3 +35,16 @@ Your Pages site will use the layout and styles from the Jekyll theme you have se
 ### Support or Contact
 
 Having trouble with Pages? Check out our [documentation](https://docs.github.com/categories/github-pages-basics/) or [contact support](https://support.github.com/contact) and we’ll help you sort it out.
+
+## Logs
+
+### 3.24
+Started researching and intergrating React. 
+To use the JSX, use
+```markdown
+npx babel --watch src --out-dir . --presets react-app/prod
+```
+in the same directory. It will auto translate JSX in the src and output to the root folder.
+
+Git comments tips: use imperative for subject line. Use the format "If applied, this commit will ..."
+i.e. "If applied, this commit will add ReactJS library and link two old projects".
--- sketch.js
+++ sketch.js
@@ -1,28 +1,46 @@
-var dx = 1000;
-var dy = 0;
+var dx = 0;
+var dy = 1000;
+var clr_timer = 60;
 
 function setup(){
-	createCanvas(400,400);
-	//frameRate(10);
+	var canv = createCanvas(windowWidth, windowHeight);
+	canv.style('display', 'block');
+	frameRate(20);
 	pixelDensity(1);
+	background(255);
+	setInterval(background, 1000 * clr_timer, 255);
+
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	background(255);
 }
 
 function draw(){
-	background(255);
 	loadPixels();
-	for (var x = 0; x < width; x++ ){
-		dx =1000;
-		for (var y = 0; y < height; y++) {
-			var index = (x + y * width)*4;
-			var bright = noise(dx,dy)*255;
-			pixels[index]=bright;
-			pixels[index+1]=bright;
-			pixels[index+2]=bright;
-			pixels[index+3]=255;
-			dx+=0.002;
-		}
-		dy += 0.002;
 
+	var x = (int) (noise(dx) * width);  // using Perlin noise for x and y
+	var y = (int) (noise(dy) * height);
+
+	console.log(width);
+	var index = (x + y * width) * 4;  	// translate 2d coord to 1d array
+
+	// width and height of the pixel dot
+	var thickness = 3;
+	for (var i = 0; i < thickness * 4; i+=4) {
+		for (var j = 0; j < thickness * 4; j += 4) {
+			var delta = j * width;
+			pixels[index+i+delta]=0;		// r
+			pixels[index+i+delta+1]=0;  	// g
+			pixels[index+i+delta+2]=0;		// b
+			pixels[index+i+delta+3]=255;	// a
+		}
 	}
+
+	dx += 0.02;				// shift the noise by 0.02 unit
+	dy += 0.02;
 	updatePixels();
+	
+
 }
